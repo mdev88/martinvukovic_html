9b359beaeb1f2e39433b72b570d188e37e45aa60
Martín Vukovic <18-martin@users.noreply.gitlab.nubeprint.net>
2024-08-17 15:05:15 -0300

    Basic structure

---
+++ script.js
@@ -0,0 +1,22 @@
+// Get the links and the content container
+const links = document.querySelectorAll('.navitem');
+const contentContainer = document.getElementById('content-container');
+
+// Add event listeners to the links
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const linkId = link.id;
+    const htmlFile = 'pages/'+linkId.replace('-link', '.html');
+    loadContent(htmlFile);
+  });
+});
+
+// Function to load the content
+function loadContent(htmlFile) {
+  fetch(htmlFile)
+    .then((response) => response.text())
+    .then((html) => {
+      contentContainer.innerHTML = html;
+    }).catch(console.log);
+}
